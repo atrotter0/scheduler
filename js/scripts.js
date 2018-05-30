0f8f383f73f1832d7dcaff154811f480f4f7f95e
Abel Trotter and Nate Cottle <abel@>
2018-05-30 16:16:05 -0700

    Add functionality to confirm button.

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,12 @@ function resetForm() {
   $("#time").val("");
 }
 
+function buildConfirmation(name, desc, date, time) {
+  $("#name-confirm").text(name);
+  $("#description-confirm").text(desc);
+  $("#date-confirm").text(date);
+  $("#time-confirm").text(time);
+}
 
 $(document).ready(function() {
   $( "#datepicker" ).datepicker();
@@ -15,5 +21,18 @@ $(document).ready(function() {
 
   $("#submit").click(function(e) {
     e.preventDefault();
+
+    var name = $("#full-name").val();
+    var description = $("#description").val();
+    var date = $("#datepicker").val();
+    var time = $("#time").val();
+    buildConfirmation(name, description, date, time);
+  });
+
+  $("#confirm").click(function() {
+    resetForm();
+    $("body").removeClass("modal-open");
+    $("#basicModal").removeClass("in");
+    $(".modal-backdrop").remove();
   });
 });
