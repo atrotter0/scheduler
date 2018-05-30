28d9a79a7ce6a187d643e39b063e641bc2b1b423
Abel Trotter and Nate Cottle <abel@>
2018-05-30 15:25:12 -0700

    Add functionality to the reset button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-$(function() {
+function resetForm() {
+  $("#full-name").val("");
+  $("#description").val("");
+  $("#datepicker").val("");
+  $("#time").val("");
+}
+
+
+$(document).ready(function() {
   $( "#datepicker" ).datepicker();
+
+  $("#reset").click(function() {
+    resetForm();
+  });
 });
